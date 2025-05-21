02d61bb0296b5af847a8eaf122cbf267b13181ef
yordan.yordanov <yordan.yordanov@diva-e.com>
2025-05-21 22:59:09 +0300

    created types

---
+++ src/types/index.ts
@@ -0,0 +1,63 @@
+interface Field {
+  type: string;
+  label: string;
+  name: string;
+  options?: string[];
+  validations?: any;
+  dependencies?: string[];
+  visibleIf?: Record<string, any>;
+  group?: string;
+  autofillAPI?: {
+    endpoint: string;
+    inputs: string[];
+  };
+  children?: Field[];
+}
+
+export interface FormSchema {
+  fields: Field[];
+}
+
+export interface DynamicFormFieldProps {
+  field: Field;
+  value: any;
+  visible: boolean;
+  onChange: (name: string, value: any) => void;
+}
+
+export interface TextareaProps {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  rows?: number;
+  placeholder?: string;
+}
+
+export interface CheckboxProps {
+  name: string;
+  label: string;
+  value: boolean;
+  onChange: (name: string, value: boolean) => void;
+}
+
+export interface DropdownFieldProps {
+  name: string;
+  label: string;
+  options: string[];
+  onChange: (name: string, value: string) => void;
+  value: string;
+}
+
+export interface JsonSchemaInputCardProps {
+  jsonInput: string;
+  setJsonInput: (value: string) => void;
+}
+
+export interface RadioButtonFieldProps {
+  name: string;
+  label: string;
+  options: string[];
+  onChange: (name: string, value: string) => void;
+  value: string;
+}
+
+
