970ae560e807f27bc7e2de478c3e0ffccd1ce220
yordan.yordanov <yordan.yordanov@diva-e.com>
2025-05-21 22:59:41 +0300

    created DynamicFormField component

---
+++ src/components/DynamicFormField.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { TextField } from '@mui/material';
+import {
+  Textarea,
+  DropdownField,
+  CheckboxField,
+  RadioButtonField,
+} from 'components';
+import { DynamicFormFieldProps } from 'types';
+
+const DynamicFormField: React.FC<DynamicFormFieldProps> = ({
+  field,
+  value,
+  visible,
+  onChange,
+}) => {
+  if (!visible) return null;
+
+  switch (field.type) {
+    case 'Text':
+      return (
+        <TextField
+          fullWidth
+          key={field.name}
+          label={field.label}
+          value={value || ''}
+          onChange={(e) => onChange(field.name, e.target.value)}
+          margin='normal'
+        />
+      );
+    case 'Textarea':
+      return (
+        <Textarea
+          value={value}
+          onChange={(e) => onChange(field.name, e.target.value)}
+          rows={4}
+          placeholder={field.label}
+        />
+      );
+    case 'Dropdown':
+      return (
+        <DropdownField
+          name={field.name}
+          label={field.label}
+          options={field.options || []}
+          value={value}
+          onChange={(name, value) => onChange(name, value)}
+        />
+      );
+    case 'Checkbox':
+      return (
+        <CheckboxField
+          name={field.name}
+          label={field.label}
+          value={value}
+          onChange={(name, value) => onChange(name, value)}
+        />
+      );
+    case 'Radio button':
+      return (
+        <RadioButtonField
+          value={value}
+          name={field.name}
+          label={field.label}
+          options={field.options || []}
+          onChange={(name, value) => onChange(name, value)}
+        />
+      );
+    default:
+      return null;
+  }
+};
+
+export default DynamicFormField;
